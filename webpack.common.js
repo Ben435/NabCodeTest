a7c4c1b2d3e79400afc6336ff82cdef7d078fcef
Ben Clare <ben.clare2@gmail.com>
2019-09-21 21:46:21 +1000

    Prod/dev configs

--- webpack.common.js
+++ webpack.common.js
@@ -2,22 +2,10 @@ const path = require('path');
 
 module.exports = {
     entry: path.resolve('src', 'main', 'js', 'app.js'),
-    devtool: 'sourcemaps',
-    cache: true,
-    mode: 'development',
+    plugins: [],
     output: {
         path: path.resolve(__dirname, 'src', 'main', 'resources', 'static', 'built'),
         filename: 'bundle.js'
-    },
-    devServer: {
-        publicPath: '/built/',
-        index: 'index.html',
-        contentBase: [path.join(__dirname, 'src', 'main', 'resources', 'static')],
-        compress: true,
-        port: 9000,
-        proxy: {
-            '/api': 'http://localhost:8080'
-        }
     },
     module: {
         rules: [
---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    devtool: 'sourcemaps',
+    cache: true,
+    mode: 'development',
+    devServer: {
+        publicPath: '/built/',
+        index: 'index.html',
+        contentBase: [path.join(__dirname, 'src', 'main', 'resources', 'static')],
+        compress: true,
+        port: 9000,
+        proxy: {
+            '/api': 'http://localhost:8080'
+        }
+    }
+});
