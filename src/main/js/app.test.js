29b16f019d2d6d4b0f3181d195c13a2c3d374297
Ben Clare <ben.clare2@gmail.com>
2019-09-22 17:05:10 +1000

    Basic frontend test

--- src/main/js/app.test.js
+++ src/main/js/app.test.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import {cleanup, render} from '@testing-library/react';
+import App from "./app";
 
 afterEach(cleanup);
 
 test('does render', () => {
-  const {} = render('<App />');
+  // Http mock
+  window.fetch = jest.fn(url => {
+    let data;
+    if (url === '/api/category') {
+      data = ['cat1, cat2'];
+    } else if (url === '/api/grocery') {
+      data = [{
+        id: '1234',
+        name: 'fruit_1',
+        category: 'fruit'
+      },{
+        id: '4321',
+        name: 'fruit_2',
+        category: 'fruit'
+      }]
+    }
+    return Promise.resolve({json: () => Promise.resolve(data)})
+  });
+
+  const {} = render(<App />);
 });
